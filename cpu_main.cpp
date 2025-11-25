6d446d8a849da2d6cf89f5a02c2c8ee838749d60
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-11-25 10:57:04 +0100

    cpu_main first commit

---
+++ cpu_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+#include "abstract_transform.hpp"
+#include "recursive_fourier.hpp"
+#include "iterative_fourier.hpp"
+#include "vector_generator.hpp"
+#include "timing_saver.h"
+#include "save_result.hpp"
+
