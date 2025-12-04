36f17e1a038049389a1812816d8b42cbd8e020b7
Alessandro Ferdinando Verrengia <alessandro.verrengia@mail.polimi.it>
2025-12-04 10:52:25 +0100

    Creating "generate input" and manage it

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,23 @@ int main(int argc, char* argv[]) {
     //Check on input arguments
     // 1 -> Select compute method (Iterative / Recursive / Parallel / All)
     // 2 -> Input file name
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <method> <input_file>" << std::endl;
+    std::string methods[4] = {"Iterative", "Recursive", "Parallel", "All"};
+    if (argc == 2) {
+        int method = std::stoi(argv[0]);
+        std::string input_file = argv[1];
+        if (method < 1 || method > 4){
+            std::cerr << "Method must be between 1 and 4, use all" << std::endl;
+            method = 4;
+        }
+
+        std::cerr << "Usage: " << methods[method-1] << " on file "<< input_file << std::endl;
+    } else if (argc == 1){
+        std::string input_file = argv[0];
+        std::cerr << "Usage: All method on file " << input_file << std::endl;
+    } else {
+        std::cerr << "Usage: <method (1-4)> <input_file>" << std::endl;
         return 1;
     }
-    int method = std::stoi(argv[1]);
 
     Fourier<double>* fft = nullptr;
     switch (method) {
@@ -23,6 +35,11 @@ int main(int argc, char* argv[]) {
             break;
         case 3:
             fft = new Parallel<double>();
+            break;
+        case 4:
+            fft1 = new Iterative<double>();
+            fft2 = new Recursive<double>();
+            fft3 = new Parallel<double>();
             break;
     }
     
