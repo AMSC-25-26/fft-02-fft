f2e7d47a41f35baa8d72d8c2da501ab0e480802b
Bianca <bianca.michielan@mail.polimi.it>
2025-12-04 12:36:45 +0100

    Add Parallel.hpp

---
+++ src/libraries/Parallel.hpp
@@ -0,0 +1,18 @@
+#include <mpi.h>
+
+template <typename T>
+class Parallel : public Fourier<T> {
+    private:
+        // Input
+        // Output
+        // Stats
+    
+
+    public:
+        void compute(const T* input, T* output) override{
+            // Implementation of parallel FFT computation
+            
+        }
+        void reverseCompute(const T* input, T* output) override;
+        void printStats() override;
+};
